6dcdfa0fadc30c5e22976cced775ced5d0f816f5
Sullivan Pierce <sullypierce@gmail.com>
2020-03-23 16:02:43 -0400

    mvp reached!

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import "./Login.css"
 import { login } from "./simpleAuth";
+import logo from '../nav/LogoMakr_4MLnWw.png'
 
 
 export default class Login extends Component {
@@ -33,36 +34,40 @@ export default class Login extends Component {
 
     render() {
       return (
-        <main>
-          <form onSubmit={this.handleLogin}>
+        <main className='loginBackground'>
+          <a href='/'><img src={logo} alt='Great Smoky Mountain Map'/></a>
+          <form className="bg-white o-80 center w-50 br4 ba" onSubmit={this.handleLogin}>
+          
             <h1 className="h3 mb3 f5 black fw2 ttu tracked">Please sign in</h1>
-            <div className="measure">
+            <div className="center measure">
               <label htmlFor="inputUsername" className="f6 b db mb2"> Username </label>
               <input onChange={this.handleInputChange}
                 type="username"
                 id="username"
-                className="input-reset ba b--black-20 pa2 mb2 db w-50"
+                className="center input-reset ba b--black-20 pa2 mb2 db w-50"
                 placeholder="Username"
                 required autoFocus />
             </div>
-            <div className="measure">
+            <div className="center measure">
               <label htmlFor="inputPassword" className="f6 b db mb2"> Password </label>
               <input onChange={this.handleInputChange}
                 type="password"
                 id="password"
-                className="input-reset ba b--black-20 pa2 mb2 db w-50"
+                className="center input-reset ba b--black-20 pa2 mb2 db w-50"
                 placeholder="Password"
                 required />
             </div>
             <div className="measure">
               <button 
                 type="submit" 
-                className='b dib f6 link br-pill ba ph2 pv1 mv3 black bg-animate hover-bg-light-pink'>
+                className='center b dib f6 link br-pill ba ph2 pv1 mv3 black bg-animate hover-bg-light-pink'>
                   Sign in
               </button>
+              <br/>
+              <a href="/register">Register New Account</a>
             </div>
           </form>
-          <a href="/register">Register New Account</a>
+         
         </main>
     )
   }
